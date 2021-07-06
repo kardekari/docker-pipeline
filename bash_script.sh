018e82cb782bc42821643ec73857bd85bbfc901f
Abdullah shaikh <abdullahshaikhneo@gmail.com>
2021-07-07 00:11:45 +0530

    bash file change

--- bash_script.sh
+++ bash_script.sh
@@ -5,13 +5,13 @@ echo "hi from bash script"
 
 echo $SSH_KEY
 
-echo $SSH_KEY >> sshfile.pem
+echo $SSH_KEY >> sshfile
 
-chmod 600 sshfile.pem
+chmod 600 sshfile
 
-cat sshfile.pem
+cat sshfile
 
-ssh -o StrictHostKeyChecking=no -i sshfile.pem ubuntu@3.108.165.10
+ssh -o StrictHostKeyChecking=no -i sshfile ubuntu@3.108.165.10
 
 ls -l
 

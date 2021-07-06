9ace54d154a7f86c714d39c1424307191addea4f
Abdullah shaikh <abdullahshaikhneo@gmail.com>
2021-07-06 23:37:41 +0530

    changes in bash file a

--- bash_script.sh
+++ bash_script.sh
@@ -2,14 +2,17 @@
 
 echo "hi from bash script"
 
-# ssh -o StrictHostKeyChecking=no -i ${SSH_KEY} ubuntu@3.108.165.10
-
-# ls -l
 
 echo $SSH_KEY
 
 echo $SSH_KEY >> sshfile
 
 cat  sshfile
+
+ssh -o StrictHostKeyChecking=no -i sshfile ubuntu@3.108.165.10
+
+ls -l
+
+exit
 
 

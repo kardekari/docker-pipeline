cc027ad789b280c87bc99c610c66f05da2ed60a7
Abdullah shaikh <abdullahshaikhneo@gmail.com>
2021-07-07 12:25:03 +0530

    chages bash file 1

--- bash_script.sh
+++ bash_script.sh
@@ -5,13 +5,15 @@ echo "hi from bash script"
 
 echo $SSH_KEY
 
-echo $SSH_KEY >> sshfile
+# echo $SSH_KEY >> sshfile
 
-chmod 600 sshfile
+# chmod 600 sshfile
 
-cat sshfile
+# cat sshfile
 
-ssh -o StrictHostKeyChecking=no -i sshfile ubuntu@3.108.165.10
+
+
+ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@3.108.165.10
 
 ls -l
 
